4a4d934bc454f8bad253aabf82027c2fb76025c2
Mohsen Mirzania <mohsen@Mohsens-MacBook-Pro.local>
2020-10-30 12:17:40 +0330

    ds

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(determineListenAddress(), nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World! from Heroku!")
+}
+
+func determineListenAddress() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return ":80"
+	}
+	return ":" + port
 }
